11520b7adf19d1d2496ec4000b2f452baed69ae0
Rohan Bhise <Rohan.Bhise@asl.mum.sst>
2023-07-17 23:24:44 +0530

    v1

---
+++ 04-book-cricket/src/com/aurionpro/test/BookCricketTest.java
@@ -0,0 +1,16 @@
+package com.aurionpro.test;
+
+import com.aurionpro.model.BookCricket;
+
+public class BookCricketTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BookCricket obj = new BookCricket();
+		System.out.println(obj.playerOneScore("rohan"));
+		System.out.println("********************************");
+		System.out.println(obj.playerTwoScore("suraj"));
+		
+	}
+
+}
